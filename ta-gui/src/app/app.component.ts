bf1d17a147164dd658439799ae27c51fca70786c
luan1221 <lssa@cin.ufpe.br>
2021-11-06 12:15:08 -0300

    evitar cadastro de mais de um aluno com o mesmo cpf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -15,9 +15,12 @@ export class AppComponent {
   alunos: Aluno[] = [];
 
   gravar(a: Aluno): void {
-    this.alunoService.gravar(a);
-    this.alunos.push(a);
-    this.aluno = {nome: "", cpf: "", email: ""};
+    if (this.alunoService.gravar(a)) {
+      this.alunos.push(a);
+      this.aluno = {nome: "", cpf: "", email: ""};
+    } else {
+      this.aluno.cpf = "";
+    }
  }
 
 }
